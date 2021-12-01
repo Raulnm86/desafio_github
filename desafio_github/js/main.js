074543e0fbee08298d17282d647945ce09268ba6
Raulnm86 <90366064+Raulnm86@users.noreply.github.com>
2021-12-01 20:36:22 -0300

    Add files via upload

---
+++ desafio_github/js/main.js
@@ -0,0 +1,7 @@
+function clicou () {
+    document.getElementById("hino").innerHTML = "<b>Ouça o hino do maior do Brasil</b>"
+}
+
+function redirecionar () {
+    window.open("https://www.youtube.com/watch?v=1H1Hp2qHBwk")
+}
